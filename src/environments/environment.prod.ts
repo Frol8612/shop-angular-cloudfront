dbcdf01de308477304eee5aa59fa6ddd23fb484a
Frol8612 <infern00730@gmail.com>
2021-08-28 19:58:53 +0300

    fix: remove empty space

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: ' https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
