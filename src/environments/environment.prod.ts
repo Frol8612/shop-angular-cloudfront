847e8a7de104462c1c8ca2c981891d327f3d7b6d
Pavel Fralou <pavel_fralou@epam.com>
2021-09-17 23:06:23 +0300

    feat: change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://hj5a9dd29b.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://y9lklhe9bc.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
