2e5f7b2cddfc6a0494576e24eef5b8bf8b6b8a0d
Pavel Fralou <pavel_fralou@epam.com>
2021-09-17 22:37:54 +0300

    feat: update path import and remove unused code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,16 +7,16 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://hj5a9dd29b.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: true,
+    product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
