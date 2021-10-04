6f345cc03282182c021e5dd980ba7c3fc3a08425
Pavel Fralou <pavel_fralou@epam.com>
2021-10-04 23:06:17 +0300

    feat: change endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://y9lklhe9bc.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://7kxq3vx6eb.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://7lnrsyjqx8.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://fe8vekrcn8.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
