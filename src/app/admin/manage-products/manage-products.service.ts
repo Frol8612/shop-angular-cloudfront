9772eba1236fecbdb2c1252c90749c717ac24033
Pavel Fralou <pavel_fralou@epam.com>
2021-10-12 16:14:00 +0300

    feat: add header

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -31,8 +31,13 @@ export class ManageProductsService extends ApiService {
 
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
+    const token = localStorage.getItem('authorization_token');
 
     return this.http.get<string>(url, {
+      headers: { 
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: `Basic ${token}`
+      },
       params: {
         name: fileName,
       },
