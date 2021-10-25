b77ac12695e39205dde65d4b966fbd7180cc8561
Frol8612 <infern00730@gmail.com>
2021-10-25 11:09:40 +0300

    feat: change endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://7lnrsyjqx8.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://fe8vekrcn8.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'http://frol8612-cart-api-dev.eba-8gzdv8c5.eu-west-1.elasticbeanstalk.com/',
+    cart: 'http://frol8612-cart-api-dev.eba-8gzdv8c5.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: false,
